e3eaccee1d80db441aee6ec44b447eddb7ae716e
drmausbaer <48391116+drmausbaer@users.noreply.github.com>
2020-02-01 14:05:47 -0500

    Update Player.js

--- Player.js
+++ Player.js
@@ -2,8 +2,8 @@ class Player{
   constructor(){
     this.pos = createVector(3*tileSize + xoff,4* tileSize + yoff);
     this.vel = createVector(0,0);
-    this.size = tileSize/2.0;
-    this.playerSpeed = tileSize/15.0;
+    this.size = tileSize*1.25/2.0;
+    this.playerSpeed = tileSize*0.8/15.0;
     this.dead = false;
     this.reachedGoal = false;
     this.fadeCounter = 255;
